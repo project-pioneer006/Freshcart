27965e9579c8dcc5c1e80940eefd2b66a03f19f9
project-pioneer006 <joeldcunha73@gmail.com>
2026-06-24 12:03:06 +0530

    Updation on server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware1
 app.use(cors()); // Allow all origins in production for now to fix connection issues
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
